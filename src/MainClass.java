eb26581e92054a78aec2fc35c44b548d4bb669cf
刘鑫宇 <23371510@buaa.edu.cn>
2025-03-25 23:44:38 +0800

    不关闭线程,改用await和signal方法

--- src/MainClass.java
+++ src/MainClass.java
@@ -5,26 +5,8 @@ import java.io.IOException;
 public class MainClass {
     public static void main(String[] args) {
         TimableOutput.initStartTimestamp();
-        ElevatorInput elevatorInput = new ElevatorInput(System.in);
-        Scheduler scheduler = Scheduler.getInstance();
-        while (true) {
-            Request request = elevatorInput.nextRequest();
-            // when request == null
-            // it means there are no more lines in stdin
-            if (request == null) {
-                break;
-            } else {
-                // a new valid request
-                if (request instanceof PersonRequest) {
-                    PersonRequest personRequest = (PersonRequest) request;
-                    scheduler.recieveRequest(personRequest);
-                }
-            }
-        }
-        try {
-            elevatorInput.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        new Thread(Input.getInstance()).start();
+        //        ElevatorInput elevatorInput = new ElevatorInput(System.in);
+//        Scheduler scheduler = Scheduler.getInstance();
     }
 }
